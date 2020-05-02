936368188ae0f588ca65031bea5806041f7c825a
sbk <seth@aims.ac.za>
2020-05-02 10:10:37 +0000

    stats

--- springer.py
+++ springer.py
@@ -18,6 +18,10 @@ def main():
 
     s = len(os.listdir('books'))
 
+    all_start = time.time()
+
+    total = 0
+
     for r, row in enumerate(table[s:]):
 
         dlink = f'https://link.springer.com/content/pdf/10.1007/{row["isbn"]}.pdf'
@@ -39,7 +43,13 @@ def main():
 
         duration = end - start
 
+        total += len(content)
+
         print(f'Download took {duration} seconds. Size downloaded {len(content)/1000000} MB ...')
+
+    duration = (all_start - time.time())//60
+
+    print(f'Download of {total/1000000000} GB completed, took {duration} minutes ...')
 
 
 main()
